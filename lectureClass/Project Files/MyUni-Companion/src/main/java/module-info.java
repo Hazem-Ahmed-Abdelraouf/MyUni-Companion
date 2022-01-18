a336ea2b5fb0d9f70a06283c8a7ccda9b5087d24
cuisaifei <80372040+cuisaifei@users.noreply.github.com>
2022-01-18 21:28:25 +0800

    Delete lectureClass directory

--- lectureClass/Project Files/MyUni-Companion/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.companion.myunicompanion {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires lombok;
-
-
-    opens org.companion.myunicompanion to javafx.fxml;
-    exports org.companion.myunicompanion;
-}
