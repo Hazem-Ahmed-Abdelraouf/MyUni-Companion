b228459b0e1bc70c715c246c3e6bd0cfa0af971a
Hazem Ahmed <hazemamn@student.usm.my>
2022-01-18 14:39:26 +0300

    reorganizing the repo

--- Project Files/MyUni-Companion/src/main/java/org/companion/myunicompanion/Main.java
+++ Project Files/MyUni-Companion/src/main/java/org/companion/myunicompanion/Main.java
@@ -3,8 +3,10 @@ package org.companion.myunicompanion;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main extends Application {
 
@@ -12,6 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.show();
 
     }
 }
